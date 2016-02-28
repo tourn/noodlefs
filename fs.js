98b6f47f1565af989f78cb9fbca3a5ee6fb46fda
Fabian Schläpfer <backslash7@gmail.com>
2016-02-28 13:52:14 +0100

    Add hacky barebone file reading capability prototype
    * Not fully working yet; open(), read() and release() need some more work

--- fs.js
+++ fs.js
@@ -1,12 +1,17 @@
 "use strict";
 
 var fuse = require('fuse-bindings')
+const fs = require('fs');
+const request = require('request');
 var util = require('util');
 var Noodle = require('./noodle')
 var auth = require('./config');
 
 // TODO: move to config, refactor to { auth: X, fuse: Y }
 var mountpoint = '/tmp/noodle';
+
+// TODO: one is not enough! base path on actual module content name to have one for each
+var tmpFilePath = '/tmp/thefile';
 
 var noodle = new Noodle(auth);
 
@@ -48,7 +53,7 @@ fuse.mount(mountpoint, {
         atime: new Date(),
         ctime: new Date(),
         size: 100,
-        mode: 16749,
+        mode: parseInt('040555', 8), 
         uid: process.getuid(),
         gid: process.getgid()
       })
@@ -59,7 +64,7 @@ fuse.mount(mountpoint, {
           atime: new Date(),
           ctime: new Date(),
           size: 100,
-          mode: 16749,
+          mode: parseInt('040555', 8), 
           uid: process.getuid(),
           gid: process.getgid()
       });
@@ -70,7 +75,8 @@ fuse.mount(mountpoint, {
           atime: new Date(),
           ctime: new Date(),
           size: 100,
-          mode: 16749,
+          //mode: 16749,
+          mode: parseInt('040555', 8), 
           uid: process.getuid(),
           gid: process.getgid()
       });
@@ -80,18 +86,21 @@ fuse.mount(mountpoint, {
         var contentName = parts[2];
         var moduleName = parts[3];
         noodle.getCourseContentModule(courseName, contentName, moduleName).then(function (module) {
+          console.log("in gettattr(): got module=" + JSON.stringify(module));
           cb(0, {
-              mtime: module.mtime,
+              mtime: module.content.mtime,
               atime: new Date(),
-              ctime: module.ctime,
-              size: module.size,
-  //            mode: 16877,
-  //            mode: 16749,
-              mode: 365, // TODO
+              ctime: module.content.ctime,
+              size: module.content.size,
+              mode: 33188,
+              //mode: parseInt('0444', 8), 
               uid: process.getuid(),
               gid: process.getgid()
           });
-        }); // TODO: catch error
+        }).catch(function (err) {
+          console.log("WARNING: received no course content module for (" + courseName + ", " + contentName + ", " + moduleName + ")");
+          cb(fuse.ENOENT)
+        });
 
     } else {
         cb(fuse.ENOENT)
@@ -100,15 +109,75 @@ fuse.mount(mountpoint, {
 
   open: function (path, flags, cb) {
     console.log('open(%s, %d)', path, flags)
-    cb(0, 42) // 42 is an fd
+
+    var parts = path.split('/');
+    if (parts.length == 4) { // course content module, aka. file
+      var courseName = parts[1];
+      var contentName = parts[2];
+      var moduleName = parts[3];
+      noodle.getCourseContentModuleUrl(courseName, contentName, moduleName).then(function (fileUrl) {
+        console.log("> open(): got file url = " + fileUrl);
+
+        request.get(fileUrl, function (error, response, body) {
+          if (!error && response.statusCode == 200) {
+            fs.writeFileSync(tmpFilePath, body);
+          }
+        }); 
+
+        var fd = fs.openSync(tmpFilePath, 'r');
+        cb(0, fd);
+
+      }).catch(function (err) {
+        console.log("WARNING: received no course content module for (" + courseName + ", " + contentName + ", " + moduleName + ")");
+        cb(fuse.ENOENT)
+      });
+    }
   },
 
   read: function (path, fd, buf, len, pos, cb) {
     console.log('read(%s, %d, %d, %d)', path, fd, len, pos)
-    var str = 'hello world\n'.slice(pos)
-    if (!str) return cb(0)
-    buf.write(str)
-    return cb(str.length)
+
+    // fs.read(fd, buffer, offset, length, position, callback)
+    // 
+    // Read data from the file specified by fd.
+    // 
+    // buffer is the buffer that the data will be written to.
+    // offset is the offset in the buffer to start writing at.
+    // length is an integer specifying the number of bytes to read.
+    // position is an integer specifying where to begin reading from in the file. If position is null, data will be read from the current file position.
+    // 
+    // The callback is given the three arguments, (err, bytesRead, buffer). 
+
+    // --------------------------------------------------------------------------
+
+    // ops.read(path, fd, buffer, length, position, cb)
+    // 
+    // Called when contents of a file is being read. You should write the result of the read to the buffer and return the number of bytes written as the first argument in the callback. If no bytes were written (read is complete) return 0 in the callback.
+    // 
+    // var data = new Buffer('hello world')
+    // 
+    // ops.read = function (path, fd, buffer, length, position, cb) {
+    //   if (position >= data.length) return cb(0) // done
+    //   var part = data.slice(position, position + length)
+    //   part.copy(buffer) // write the result of the read to the result buffer
+    //   cb(part.length) // return the number of bytes read
+    // }
+
+    console.log("typeof buf = " + typeof buf);
+
+    fs.read(fd, buf, 0, len, pos, function (err, bytesRead, buffer) {
+      console.log("fs.read err=" + err + " bytesRead=" + bytesRead);
+      cb(bytesRead);
+    });
+  },
+
+  release: function(path, fd, cb) {
+    console.log('release(%s, %d)', path, fd);
+
+    fs.closeSync(fd);
+    fs.unlinkSync(tmpFilePath);
+
+    // TODO: hangs/blocks, file probably still in use somewhere
   }
 });
 
--- noodle.js
+++ noodle.js
@@ -3,17 +3,6 @@
 var assert = require('assert');
 var moodle_client = require("moodle-client");
 var Promise = require('bluebird');
-var auth = require('./config');
-
-var moodle = function(callback){
-  console.log("Initialize moodle client");
-  moodle_client.init(auth).then(function(client) {
-        callback(client);
-
-  }).catch(function(err) {
-        console.log("Unable to initialize the client: " + err);
-  });
-}
 
 function Noodle(auth) {
   var self = this;
@@ -51,7 +40,7 @@ function Noodle(auth) {
   self.init();
 
   self.getCourseContentNames = function(courseName){
-    console.log("getCourseContentNames " + courseName + " id=" + self.courses[courseName].id);
+    console.log("> getCourseContentNames " + courseName + " id=" + self.courses[courseName].id);
     return new Promise(function(resolve,reject){
       moodle_client.init(auth).then(function(client) {
         client.call({
@@ -87,20 +76,26 @@ function Noodle(auth) {
           args: { courseid: self.courses[courseName].id },
         }).then(function(courseContents){
           var modules = [];
+
+          // find all modules in course content that have at least one file as their content, and return all those files, if any
           courseContents.forEach(function (content) {
             console.log(content);
             if (content.name === contentName && content.modules.length >= 1) {
-              modules = content.modules.map(function (module) {
-                return {
-                  name: module.name,
-                  ctime: module.timecreated,
-                  mtime: module.timemodified,
-                  size: 42,
-                  type: module.modname
+              modules = [];
+              content.modules.forEach(function (module) {
+                if (module.contents !== undefined && module.contents.length >= 1 && module.contents[0].type === 'file') {
+                  modules.push({
+                    name: module.name,
+                    ctime: module.contents[0].timecreated,
+                    mtime: module.contents[0].timemodified,
+                    size: module.contents[0].filesize,
+                    type: module.modname
+                  });
                 }
               });
             }
           });
+
           resolve(modules);
         });
 
@@ -110,11 +105,12 @@ function Noodle(auth) {
     });
   };
 
-  // TODO: DRY these two functions
+  // TODO: DRY these two functions (above and below)
   
+  // TODO: rename these to include "file" (limitation for now)
   self.getCourseContentModule = function(courseName, contentName, moduleName) {
-    console.log("getCourseContentModule " + courseName);
-    console.log("   id=" + self.courses[courseName].id);
+    console.log("> getCourseContentModule " + courseName);
+    console.log(">   id=" + self.courses[courseName].id);
     return new Promise(function(resolve,reject){
       moodle_client.init(auth).then(function(client) {
         client.call({
@@ -122,22 +118,90 @@ function Noodle(auth) {
           args: { courseid: self.courses[courseName].id },
         }).then(function(courseContents){
           var requestedModule = {};
+
           courseContents.forEach(function (content) {
             if (content.name === contentName && content.modules.length >= 1) {
-              for (var module in content.modules) {
+            console.log("> found content");
+              content.modules.forEach(function (module) {
+                  console.log("> check module " + module.name + " for match with req " + moduleName);
                 if (module.name === moduleName) {
-                  requestedModule = { // TODO: DRY, write a function `parseModule`
+                  console.log(">> found module = " + JSON.stringify(module));
+
+                  // >> found module = {"id":17964,"url":"https://moodle.hsr.ch/mod/resource/view.php?id=17964","name":"Zusatzmaterial zu U1 - Dokumentation ...","instance":8381,"visible":1,"modicon":"https://moodle.hsr.ch/theme/image.php/_s/more/core/1455119128/f/archive-24","modname":"resource","modplural":"Dateien","indent":0,
+                  // "contents":[
+                  //    {"type":"file","filename":"uebung-dokumentation.zip","filepath":"/","filesize":2464250,"fileurl":"https://moodle.hsr.ch/webservice/pluginfile.php/35098/mod_resource/content/1/uebung-dokumentation.zip?forcedownload=1","timecreated":1456155628,"timemodified":1456155652,"sortorder":1,"userid":3256,"author":"Daniel Keller","license":"allrightsreserved"}
+                  // ]}
+                  
+                  if (module.contents === undefined || module.contents.length == 0 || module.contents[0].type !== 'file') {
+                    reject("Not a file");
+                  }
+
+                  // TODO: DRY, write a function `parseModule`
+                  // only works for files (i'm assuming)
+                  var modContent = {
+                    type: module.contents[0].type,
+                    size: module.contents[0].filesize,
+                    ctime: module.contents[0].timecreated,
+                    mtime: module.contents[0].timemodified
+                  }
+                  requestedModule = {
                     name: module.name,
-                    ctime: module.timecreated,
-                    mtime: module.timemodified,
-                    size: 42,
-                    type: module.modname
+                    type: module.modname,
+                    content: modContent
                   }
                 }
-              }
+              });
             }
           });
+
           resolve(requestedModule);
+        });
+
+      }).catch(function(err) {
+            console.log("Unable to initialize the client: " + err);
+      });
+    });
+  };
+
+  self.getCourseContentModuleUrl = function(courseName, contentName, moduleName) {
+    console.log("> getCourseContentModule " + courseName);
+    console.log(">   id=" + self.courses[courseName].id);
+    return new Promise(function(resolve,reject){
+      moodle_client.init(auth).then(function(client) {
+        client.call({
+          wsfunction: "core_course_get_contents",
+          args: { courseid: self.courses[courseName].id },
+        }).then(function(courseContents){
+          var fileUrl = '';
+
+          courseContents.forEach(function (content) {
+            if (content.name === contentName && content.modules.length >= 1) {
+            console.log("> found content");
+              content.modules.forEach(function (module) {
+                  console.log("> check module " + module.name + " for match with req " + moduleName);
+                if (module.name === moduleName) {
+                  console.log(">> found module = " + JSON.stringify(module));
+
+                  // >> found module = {"id":17964,"url":"https://moodle.hsr.ch/mod/resource/view.php?id=17964","name":"Zusatzmaterial zu U1 - Dokumentation ...","instance":8381,"visible":1,"modicon":"https://moodle.hsr.ch/theme/image.php/_s/more/core/1455119128/f/archive-24","modname":"resource","modplural":"Dateien","indent":0,
+                  // "contents":[
+                  //    {"type":"file","filename":"uebung-dokumentation.zip","filepath":"/","filesize":2464250,"fileurl":"https://moodle.hsr.ch/webservice/pluginfile.php/35098/mod_resource/content/1/uebung-dokumentation.zip?forcedownload=1","timecreated":1456155628,"timemodified":1456155652,"sortorder":1,"userid":3256,"author":"Daniel Keller","license":"allrightsreserved"}
+                  // ]}
+                  
+                  if (module.contents === undefined || module.contents.length == 0 || module.contents[0].type !== 'file') {
+                    reject("Not a file");
+                  }
+
+                  fileUrl = module.contents[0].fileurl + "&token=" + auth.token;
+                }
+              });
+            }
+          });
+
+          if (fileUrl === '') {
+              reject("No URL for file found!");
+          }
+
+          resolve(fileUrl);
         });
 
       }).catch(function(err) {
